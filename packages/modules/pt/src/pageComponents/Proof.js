c20874ed6298ca2082140d6ff9049d69a4012715
vamshikrishnakole-wtt-egov <57343517+vamshikrishnakole-wtt-egov@users.noreply.github.com>
2021-04-10 08:04:06 +0530

    RAIN-2418 (PT Citizen ::  Intermittent Issue :: 'Choose file' button is navigating to next screen while creating Property application) (#613)

--- packages/modules/pt/src/pageComponents/Proof.js
+++ packages/modules/pt/src/pageComponents/Proof.js
@@ -57,6 +57,7 @@ const Proof = ({ t, config, onSelect, userType, formData }) => {
       <CardLabelDesc>{t(`PT_UPLOAD_RESTRICTIONS_TYPES`)}</CardLabelDesc>
       <CardLabelDesc>{t(`PT_UPLOAD_RESTRICTIONS_SIZE`)}</CardLabelDesc>
       <UploadFile
+        extraStyleName={"propertyCreate"}
         accept=".jpg"
         onUpload={selectfile}
         onDelete={() => {
--- packages/react-components/src/atoms/UploadFile.js
+++ packages/react-components/src/atoms/UploadFile.js
@@ -2,14 +2,58 @@ import React, { useEffect, useRef, useState } from "react";
 import ButtonSelector from "./ButtonSelector";
 import { Close } from "./svgindex";
 
+const getCitizenStyles = (value) => {
+  let citizenStyles = {};
+  if (value == "propertyCreate") {
+    citizenStyles = {
+      textStyles: {
+        whiteSpace: "nowrap",
+        width: "100%",
+        overflow: "hidden",
+        textOverflow: "ellipsis"
+      },
+      tagStyles: {
+        width: "90%"
+      },
+      inputStyles: {
+        width: "44%"
+      },
+      buttonStyles: {
+        height: "auto",
+        minHeight: "2rem",
+        width: "40%"
+      },
+      tagContainerStyles: {
+        width: "60%"
+      }
+    }
+  } else {
+    citizenStyles = {
+      textStyles: {},
+      tagStyles: {},
+      inputStyles: {},
+      buttonStyles: {},
+      tagContainerStyles: {}
+    }
+  }
+  return citizenStyles;
+}
+
 const UploadFile = (props) => {
   const inpRef = useRef();
   const [hasFile, setHasFile] = useState(false);
-
+  let extraStyles = {};
   const handleChange = () => {
     if (inpRef.current.files[0]) setHasFile(true);
     else setHasFile(false);
   };
+  switch(props.extraStyleName) {
+    case "propertyCreate":
+      extraStyles = getCitizenStyles("propertyCreate");
+    break;
+    default: 
+      extraStyles = getCitizenStyles("");
+  }
 
   const handleDelete = () => {
     inpRef.current.value = "";
@@ -21,13 +65,13 @@ const UploadFile = (props) => {
   return (
     <div className="upload-file">
       <div>
-        <ButtonSelector theme="border" label="Choose File" />
+        <ButtonSelector theme="border" label="Choose File" style={extraStyles ? extraStyles?.buttonStyles : null} />
         {!hasFile ? (
           <h2 className="file-upload-status">{props.message}</h2>
         ) : (
-          <div className="tag-container">
-            <div className="tag">
-              <span className="text">{inpRef.current.files[0]?.name?.slice(0, 20)}</span>
+          <div className="tag-container" style={extraStyles ? extraStyles?.tagContainerStyles : null}>
+            <div className="tag" style={extraStyles ? extraStyles?.tagStyles : null}>
+              <span className="text" style={extraStyles ? extraStyles?.textStyles : null}>{inpRef.current.files[0]?.name?.slice(0, 20)}</span>
               <span onClick={() => handleDelete()}>
                 <Close className="close" />
               </span>
@@ -35,7 +79,7 @@ const UploadFile = (props) => {
           </div>
         )}
       </div>
-      <input ref={inpRef} type="file" name="file" accept={props.accept} onChange={(e) => props.onUpload(e)} />
+      <input style={extraStyles ? extraStyles?.inputStyles : null} ref={inpRef} type="file" name="file" accept={props.accept} onChange={(e) => props.onUpload(e)} />
     </div>
   );
 };
