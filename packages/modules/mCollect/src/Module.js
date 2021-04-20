571a7aad6eff51b8700476721803b59e10f55657
Priyanka <priyanka@egovernments.org>
2021-04-20 15:50:00 +0530

    Pushing new module mCollect

---
+++ packages/modules/mCollect/src/Module.js
@@ -0,0 +1,52 @@
+import { Header, HomeLink } from "@egovernments/digit-ui-react-components";
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useRouteMatch } from "react-router-dom";
+import EmployeeApp from "./pages/employee";
+
+const componentsToRegister = {};
+
+const addComponentsToRegistry = () => {
+  Object.entries(componentsToRegister).forEach(([key, value]) => {
+    Digit.ComponentRegistryService.setComponent(key, value);
+  });
+};
+
+export const mCollectModule = ({ userType, tenants }) => {
+  const moduleCode = "mCollect";
+  addComponentsToRegistry();
+  console.log(moduleCode, "module integrated");
+  // Digit.SessionStorage.set("PT_TENANTS", tenants);
+
+  const { path, url } = useRouteMatch();
+
+  if (userType === "employee") {
+    return <EmployeeApp path={path} url={url} userType={userType} />;
+  } else return <CitizenApp />;
+};
+
+export const mCollectLinks = ({ matchPath, userType }) => {
+  const { t } = useTranslation();
+  // const [params, setParams, clearParams] = Digit.Hooks.useSessionStorage("PT_CREATE_PROPERTY", {});
+
+  useEffect(() => {
+    clearParams();
+  }, []);
+
+  // return (
+  //   <React.Fragment>
+  //     <Header>{t("ACTION_TEST_PROPERTY_TAX")}</Header>
+  //     <div className="d-grid">
+  //       <HomeLink to={`${matchPath}/property/new-application`}>{t("PT_CREATE_PROPERTY")}</HomeLink>
+  //       <HomeLink to={`${matchPath}/property/my-properties`}>{t("PT_MY_PROPERTIES")}</HomeLink>
+  //       <HomeLink to={`${matchPath}/property/my-applications`}>{t("PT_MY_APPLICATION")}</HomeLink>
+  //     </div>
+  //   </React.Fragment>
+  // );
+};
+
+export const mCollectComponents = {
+  mCollectCard,
+  mCollectModule,
+  mCollectLinks,
+};
