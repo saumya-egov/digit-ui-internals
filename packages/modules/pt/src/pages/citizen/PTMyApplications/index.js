29354e087c8e239e6c2519e8dba5d7115b66964d
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-04-20 19:09:43 +0530

    RAIN-2479 : PT Updated the Localisation code for the my application and proeprty screens (#647)

--- packages/modules/pt/src/pages/citizen/PTMyApplications/index.js
+++ packages/modules/pt/src/pages/citizen/PTMyApplications/index.js
@@ -31,7 +31,7 @@ export const PTMyApplications = () => {
         <p style={{ marginLeft: "16px", marginTop: "16px" }}>
           {t("PT_TEXT_NOT_ABLE_TO_FIND_THE_APPLICATION")}{" "}
           <span className="link">
-            <Link to="/digit-ui/citizen/pt/property/new-application/info">{t("PT_TEXT_WANT_TO_ADD_A_NEW_PROPERTY")}</Link>
+            <Link to="/digit-ui/citizen/pt/property/new-application/info">{t("PT_COMMON_CLICK_HERE_TO_REGISTER_NEW_PROPERTY")}</Link>
           </span>
         </p>
       </div>
