11374b2a41738720dc1fffc163b2ca8311fd950d
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-05-04 18:07:52 +0530

    RAIN-2624 : Common PAy Arrears Tab alignment issue fixed (#739)

--- packages/modules/common/src/payments/citizen/bills/routes/bill-details/arrear-table.js
+++ packages/modules/common/src/payments/citizen/bills/routes/bill-details/arrear-table.js
@@ -13,10 +13,18 @@ const styles = {
         backgroundColor: 'rgba(250, 250, 250, var(--bg-opacity))'
     },
     cell: {
-        maxWidth: '1.5em',
+        maxWidth: '4em',
         minWidth: '1em',
         border: '1px solid #e8e7e6',
-        padding: '4px 10px'
+        padding: '4px 5px',
+        fontSize: '0.8em',
+        textAlign: 'left',
+        lineHeight: '1.5em'
+    
+    },
+    cellHeader:{
+        overflow: 'hidden',
+        textOverflow: 'ellipsis'
     },
     cellLeft:{
         // position: 'sticky',
@@ -42,12 +50,12 @@ const ArrearTable = ({
                 <table className={className} style={styles.table}>
                     <thead>
                         <tr>
-                            <th style={{ ...styles.cell, maxWidth:'2em',...styles.cellLeft}}>
+                            <th style={{ ...styles.cell,...styles.cellLeft,...styles.cellHeader}}>
                                 {t('CS_BILL_PERIOD')}
                             </th>
                             {headers.map((header, ind) => {
                                 let styleRight=headers.length==ind+1?styles.cellRight:{};
-                                return (<th style={{...styles.cell,...styleRight}} key={ind} >{t(header)}</th>)
+                                return (<th style={{...styles.cell,...styleRight ,...styles.cellHeader}} key={ind} >{t(header)}</th>)
 
                             })}
                         </tr>
@@ -55,20 +63,20 @@ const ArrearTable = ({
                     <tbody>
                         {Object.values(values).map((row, ind) => (
                             <tr key={ind}>
-                                <td style={{ ...styles.cell, maxWidth:'2em',...styles.cellLeft}} component="th" scope="row">{Object.keys(values)[ind]}</td>
+                                <td style={{ ...styles.cell,...styles.cellLeft}} component="th" scope="row">{Object.keys(values)[ind]}</td>
                                 {headers.map((header, i) => {
                                      let styleRight=headers.length==i+1?styles.cellRight:{};
-                                    return (<td style={{ ...styles.cell, textAlign: 'right',...styleRight,  whiteSpace: 'pre' }} key={i} numeric>₹{row[header] && row[header]['value'] || '0'}</td>)
+                                    return (<td style={{ ...styles.cell, textAlign: 'left',...styleRight,  whiteSpace: 'pre' }} key={i} numeric>₹{row[header] && row[header]['value'] || '0'}</td>)
                                 })}
                             </tr>
                         ))}
                         <tr>
-                            <td style={{...styles.cell,...styles.cellLeft,maxWidth:'2em'}}></td>
+                            <td style={{...styles.cell,...styles.cellLeft}}></td>
                             {headers.map((header, ind) => {
                                 if (ind == headers.length - 1) {
-                                    return (<td style={{ ...styles.cell,...styles.cellRight, textAlign: 'right' ,fontWeight: '700' ,  whiteSpace: 'pre'}} key={ind} numeric>{arrears}</td>)
+                                    return (<td style={{ ...styles.cell,...styles.cellRight, textAlign: 'left' ,fontWeight: '700' ,  whiteSpace: 'pre'}} key={ind} numeric>{arrears}</td>)
                                 } else if (ind == headers.length - 2) {
-                                    return (<td style={{ ...styles.cell, textAlign: 'right' }} key={ind} numeric>{t('COMMON_ARREARS_TOTAL')}</td>)
+                                    return (<td style={{ ...styles.cell, textAlign: 'left' }} key={ind} numeric>{t('COMMON_ARREARS_TOTAL')}</td>)
                                 }
                                 else {
                                     return (<td style={styles.cell} key={ind} numeric></td>)
