0b62e34236111a261824e0362dab1952e01f21e5
tulika kundu <tulika.kundu@egovernments.org>
2021-05-06 13:28:54 +0530

    fixing the error

--- packages/modules/pt/src/pages/citizen/EditProperty/index.js
+++ packages/modules/pt/src/pages/citizen/EditProperty/index.js
@@ -70,7 +70,7 @@ const getPropertyEditDetails = (data = {}) => {
   data.address.city = { code: data?.tenantId };
   data.address.locality.i18nkey = data?.tenantId.replace(".", "_").toUpperCase() + "_" + "REVENUE" + "_" + data?.address?.locality?.code;
   let addressDocs = data?.documents?.filter((doc) => doc?.documentType?.includes("ADDRESSPROOF"));
-  addressDocs[0]?.documentType = { code: addressDocs[0]?.documentType, i18nKey: stringReplaceAll(addressDocs[0]?.documentType, ".", "_") };
+  addressDocs[0].documentType = { code: addressDocs[0]?.documentType, i18nKey: stringReplaceAll(addressDocs[0]?.documentType, ".", "_") };
   if (data?.address?.documents) {
     data.address.documents["ProofOfAddress"] = addressDocs[0];
   } else {
