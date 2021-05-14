1328ebc4623f7efbbb817c68e051ae66771e8c50
saurabh-egov <saurabh.s@egovernments.org>
2021-05-14 23:17:36 +0530

    added multi select dropdown and made it functional

--- packages/modules/dss/src/components/Filters.js
+++ packages/modules/dss/src/components/Filters.js
@@ -1,17 +1,36 @@
-import React, { useContext, useState } from "react";
-import { CardSectionHeader, Dropdown, SubmitBar, FilterIcon, RefreshIcon } from "@egovernments/digit-ui-react-components";
+import React, { useContext, useEffect, useState } from "react";
+import { MultiSelectDropdown } from "@egovernments/digit-ui-react-components";
 import Switch from "./Switch";
 import DateRange from "./DateRange";
 import FilterContext from "./FilterContext";
 
 const Filters = ({ t, ulbTenants }) => {
   const { value, setValue } = useContext(FilterContext);
+
+  const [selected, setSelected] = useState(() =>
+    ulbTenants.filter((tenant) => value.filters.tenantId.find((selectedTenant) => selectedTenant === tenant.code))
+  );
+
+  useEffect(() => {
+    setSelected(ulbTenants.filter((tenant) => value.filters.tenantId.find((selectedTenant) => selectedTenant === tenant.code)));
+  }, [value.filters.tenantId]);
+
   const selectULB = (data) => {
-    setValue({ ...value, filters: { tenantId: [data.code] } });
+    setValue({ ...value, filters: { tenantId: [...value?.filters?.tenantId, data] } });
+  };
+  const removeULB = (data) => {
+    setValue({ ...value, filters: { ...value?.filters, tenantId: [...value?.filters?.tenantId].filter((tenant) => tenant !== data) } });
   };
   const handleFilterChange = (data) => {
     setValue({ ...value, ...data });
   };
+
+  const selectFilters = (e, data) => {
+    const { checked } = e?.target;
+    if (checked) selectULB(data.code);
+    else removeULB(data.code);
+  };
+
   return (
     <div className="filters-wrapper">
       <div className="filters-input">
@@ -19,16 +38,29 @@ const Filters = ({ t, ulbTenants }) => {
       </div>
       <div className="filters-input">
         <div>{t("ES_DSS_DDR")}</div>
-        <Dropdown option={ulbTenants} optionKey="ddrKey" select={selectULB} />
+        <MultiSelectDropdown
+          options={ulbTenants}
+          optionsKey="ddrKey"
+          onSelect={selectFilters}
+          selected={selected}
+          defaultLabel={t("ES_DSS_ALL_DDR_SELECTED")}
+          defaultUnit={t("ES_DSS_DDR_SELECTED")}
+        />
       </div>
       <div className="filters-input">
         <div>{t("ES_DSS_ULB")}</div>
-        <Dropdown option={ulbTenants} optionKey="ulbKey" select={selectULB} />
+        <MultiSelectDropdown
+          options={ulbTenants}
+          optionsKey="ulbKey"
+          onSelect={selectFilters}
+          selected={selected}
+          defaultLabel={t("ES_DSS_ALL_ULB_SELECTED")}
+          defaultUnit={t("ES_DSS_ULB_SELECTED")}
+        />
       </div>
       <div className="filters-input" style={{ flexBasis: "16%" }}>
         <Switch onSelect={handleFilterChange} />
       </div>
-      {/* <SubmitBar label={"Apply"} /> */}
     </div>
   );
 };
