1b28a456934106912baea6e39512366b31f5778b
Your Name <you@example.com>
2021-05-16 20:01:49 +0530

    @shreya added echallan integration

--- packages/modules/mCollect/src/EmployeeChallan.js
+++ packages/modules/mCollect/src/EmployeeChallan.js
@@ -5,21 +5,16 @@ import { useParams } from "react-router-dom";
 import { propertyCardBodyStyle } from "../../../modules/pt/src/utils";
 
 const EmployeeChallan = (props) => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   const { challanno } = useParams();
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const coreData = Digit.Hooks.useCoreData();
-  const challans = challan || {};
   const { isLoading, isError, error, data, ...rest } = Digit.Hooks.mcollect.useMCollectSearch({ tenantId, filters: { challanno } });
-  const challan = data?.Properties[0];
-  const totaldueamount = compensation + fieldfee + securitydeposit + cgst + sgst + roundoff;
-  let units = [];
-  units = challan?.units;
-  let owners = [];
-  owners = challan?.owners;
-  let docs = [];
-  docs = challan?.documents;
-
+  console.log(data?.challans);
+  var challanDetails = data?.challans?.filter(function (item) {
+    return item.challanNo === challanno;
+  })[0];
+  // console.log(challanDetails)
   return (
     <React.Fragment>
       <div style={{ width: "30%", fontFamily: "calibri", color: "#FF0000" }}>
@@ -29,32 +24,32 @@ const EmployeeChallan = (props) => {
         <Card>
           <CardSubHeader>Challan No : {challanno} </CardSubHeader>
           <StatusTable>
-            <Row label={"Compensation of lieu of concessions"} text={`${t(challans?.citizen?.compensation)}` || "NA"} />
-            <Row label={"Field Fee"} text={"₹500"} text={`${t(challans?.citizen?.fieldfee)}` || "NA"} />
-            <Row label={"Security Deposit"} text={`${t(challans?.citizen?.securitydeposit)}` || "NA"} />
-            <Row label={"CGST"} text={`${t(challans?.citizen?.cgst)}` || "NA"} />
-            <Row label={"SGST"} text={`${t(challans?.citizen?.sgst)}` || "NA"} />
-            <Row label={"Round Off"} text={`${t(challans?.citizen?.roundoff)}` || "NA"} />
+            <Row label={"Compensation of lieu of concessions"} text={"₹5000"} textStyle={{ whiteSpace: "pre" }} />
+            <Row label={"Field Fee"} text={"₹500"} textStyle={{ whiteSpace: "pre" }} />
+            <Row label={"Security Deposit"} text="₹50" />
+            <Row label={"CGST"} text={`${"₹20"}` || "NA"} />
+            <Row label={"SGST"} text={`${"₹20"}` || "NA"} />
+            <Row label={"Round Off"} text={`${"₹0"}` || "NA"} />
             <hr />
-            <Row label={<b>Total Due Amount</b>} text={`${t(totaldueamount)}` || "NA"} />
+            <Row label={<b>Total Due Amount</b>} text={challanDetails?.amount} />
           </StatusTable>
           <Card>
             <CardSubHeader>{"SERVICE DETAILS"}</CardSubHeader>
             <StatusTable>
-              <Row label={"UC_SERVICE_CATEGORY_LABEL"} text={`${t(challans?.citizen?.businessService)}` || "NA"} />
-              <Row label={"UC_FROM_DATE_LABEL"} text={`${t(challans?.citizen?.taxPeriodFrom)}` || "NA"} />
-              <Row label={"UC_TO_DATE_LABEL"} text={`${t(challans?.citizen?.taxPeriodTo)}` || "NA"} />
+              <Row label={"UC_SERVICE_CATEGORY_LABEL"} text={challanDetails?.businessService} textStyle={{ whiteSpace: "pre" }} />
+              <Row label={"UC_FROM_DATE_LABEL"} text={challanDetails?.taxPeriodFrom} />
+              <Row label={"UC_TO_DATE_LABEL"} text={challanDetails?.taxPeriodTo} />
               <Row label={"UC_COMMENT_LABEL"} text={`${"Sample Comments, Address"}` || "NA"} />
-              <Row label={"CS_INBOX_STATUS_FILTER"} text={`${"Active"}` || "NA"} />
+              <Row label={"CS_INBOX_STATUS_FILTER"} text={challanDetails?.applicationStatus} />
             </StatusTable>
             <CardSubHeader>{"CONSUMER DETAILS"}</CardSubHeader>
             <StatusTable>
-              <Row label={"UC_CONS_NAME_LABEL"} text={`${t(challans?.citizen?.name)}` || "NA"} />
-              <Row label={"UC_MOBILE_NO_LABEL"} text={`${t(challans?.citizen?.mobileNumber)}` || "NA"} />
-              <Row label={"UC_DOOR_NO_LABEL"} text={`${t(challans?.citizen?.address?.doorNo)}` || "NA"} />
-              <Row label={"UC_BLDG_NAME_LABEL"} text={`${t(challans?.citizen?.address?.buildingName)}` || "NA"} />
-              <Row label={"UC_SRT_NAME_LABEL"} text={`${t(challans?.citizen?.address?.street)}` || "NA"} />
-              <Row label={"UC_MOHALLA_LABEL"} text={`${t(challans?.citizen?.address?.district)}` || "NA"} />
+              <Row label={"UC_CONS_NAME_LABEL"} text={challanDetails?.citizen.name} />
+              <Row label={"UC_MOBILE_NO_LABEL"} text={challanDetails?.citizen.mobileNumber} />
+              <Row label={"UC_DOOR_NO_LABEL"} text={challanDetails?.address.doorNo} />
+              <Row label={"UC_BLDG_NAME_LABEL"} text={challanDetails?.address.buildingName} />
+              <Row label={"UC_SRT_NAME_LABEL"} text={challanDetails?.address.street} />
+              <Row label={"UC_MOHALLA_LABEL"} text={challanDetails?.address.district} />
             </StatusTable>
           </Card>
         </Card>
