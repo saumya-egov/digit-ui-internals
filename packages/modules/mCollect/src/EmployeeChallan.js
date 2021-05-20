97120caa3e12260a64bae8c58852a68e7777c09f
vamshikrishnakole-wtt-egov <57343517+vamshikrishnakole-wtt-egov@users.noreply.github.com>
2021-05-20 12:49:52 +0530

    RAIN-2528 UI: mCollect Employee Challan Information screen static changes and integration with challan search api (#793)

--- packages/modules/mCollect/src/EmployeeChallan.js
+++ packages/modules/mCollect/src/EmployeeChallan.js
@@ -1,57 +1,69 @@
-import { Card, CardSubHeader, Header, LinkButton, Loader, Row, StatusTable } from "@egovernments/digit-ui-react-components";
-import React from "react";
+import { Card, CardSubHeader, Header, Row, StatusTable } from "@egovernments/digit-ui-react-components";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
-import { propertyCardBodyStyle } from "../../../modules/pt/src/utils";
+import { stringReplaceAll, convertEpochToDate } from "./utils";
 
 const EmployeeChallan = (props) => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const { challanno } = useParams();
   const tenantId = Digit.ULBService.getCurrentTenantId();
-  const coreData = Digit.Hooks.useCoreData();
-  const { isLoading, isError, error, data, ...rest } = Digit.Hooks.mcollect.useMCollectSearch({ tenantId, filters: { challanno } });
-  console.log(data?.challans);
+  const [challanBillDetails, setChallanBillDetails] = useState([]);
+  const [totalDueAmount, setTotalDueAmount] = useState(0);
+  const { isLoading, isError, error, data, ...rest } = Digit.Hooks.mcollect.useMCollectSearch({ tenantId, filters: { challanNo: challanno } });
   var challanDetails = data?.challans?.filter(function (item) {
     return item.challanNo === challanno;
   })[0];
-  // console.log(challanDetails)
+  let billDetails = [];
+  useEffect(() => {
+    async function fetchMyAPI() {
+      billDetails = [];
+      let res = await Digit.PaymentService.searchBill(tenantId, { consumerCode: data?.challans[0]?.challanNo, service: data?.challans[0]?.businessService });
+      res?.Bill[0]?.billDetails[0]?.billAccountDetails?.map(bill => {
+        billDetails.push(bill);
+      });
+      setTotalDueAmount(res?.Bill[0]?.totalAmount);
+      setChallanBillDetails(billDetails);
+      console.log(res, "resresresres")
+    }
+    if (data?.challans && data?.challans?.length > 0) {
+      fetchMyAPI();
+    }
+  }, [data]);
+
   return (
     <React.Fragment>
       <div style={{ width: "30%", fontFamily: "calibri", color: "#FF0000" }}>
-        <Header>CHALLAN DETAILS </Header>
+        <Header>{`${t("CHALLAN_DETAILS")}`} </Header>
       </div>
-      <div style={{ ...propertyCardBodyStyle, maxHeight: "calc(100vh - 12em)", margin: "30px" }}>
+      <div>
         <Card>
-          <CardSubHeader>Challan No : {challanno} </CardSubHeader>
-          <StatusTable>
-            <Row label={"Tenant id"} text={"₹5000"} textStyle={{ whiteSpace: "pre" }} />
-            <Row label={"Bill Detail Id"} text={"₹500"} textStyle={{ whiteSpace: "pre" }} />
-            <Row label={"Demand Detail id"} text="₹50" />
-            <Row label={"Tax Head code"} text={`${"₹20"}` || "NA"} />
-            <Row label={"Adjusted amount"} text={`${"₹20"}` || "NA"} />
-            <Row label={"Order"} text={`${"₹0"}` || "NA"} />
-            <hr />
-            <Row label={<b>Total Due Amount</b>} text={challanDetails?.amount} />
+          <CardSubHeader>{t("UC_CHALLAN_NO")} : {challanno} </CardSubHeader>
+          <hr style={{ width: "34%", border: "1px solid #D6D5D4" }} />
+          <StatusTable style={{ padding: "10px 0px" }}>
+            {challanBillDetails?.map(data => {
+              return <Row label={t(stringReplaceAll(data?.taxHeadCode, ".", "_"))} text={`₹${data?.amount}`} textStyle={{ whiteSpace: "pre" }} />
+            })}
+            <hr style={{ width: "34%", border: "1px solid #D6D5D4" }} />
+            <Row label={<b style={{ padding: "10px 0px" }}>{t("UC_TOTAL_DUE_AMOUT_LABEL")}</b>} text={`₹${totalDueAmount}`} />
           </StatusTable>
-          <Card>
-            <CardSubHeader>{"SERVICE DETAILS"}</CardSubHeader>
-            <StatusTable>
-              <Row label={"UC_SERVICE_CATEGORY_LABEL"} text={challanDetails?.businessService} textStyle={{ whiteSpace: "pre" }} />
-              <Row label={"UC_FROM_DATE_LABEL"} text={challanDetails?.taxPeriodFrom} />
-              <Row label={"UC_TO_DATE_LABEL"} text={challanDetails?.taxPeriodTo} />
-              <Row label={"UC_COMMENT_LABEL"} text={`${"Sample Comments, Address"}` || "NA"} />
-              <Row label={"CS_INBOX_STATUS_FILTER"} text={challanDetails?.applicationStatus} />
-            </StatusTable>
-            <CardSubHeader>{"CONSUMER DETAILS"}</CardSubHeader>
-            <StatusTable>
-              <Row label={"UC_CONS_NAME_LABEL"} text={challanDetails?.citizen.name} />
-              <Row label={"UC_MOBILE_NO_LABEL"} text={challanDetails?.citizen.mobileNumber} />
-              <Row label={"UC_DOOR_NO_LABEL"} text={challanDetails?.address.doorNo} />
-              <Row label={"UC_BLDG_NAME_LABEL"} text={challanDetails?.address.buildingName} />
-              <Row label={"UC_SRT_NAME_LABEL"} text={challanDetails?.address.street} />
-              <Row label={"UC_MOHALLA_LABEL"} text={challanDetails?.address.district} />
-            </StatusTable>
-          </Card>
+          <CardSubHeader>{t("UC_SERVICE_DETAILS_LABEL")}</CardSubHeader>
+          <StatusTable>
+            <Row label={`${t("UC_SERVICE_CATEGORY_LABEL")}:`} text={`${t(`BILLINGSERVICE_BUSINESSSERVICE_${stringReplaceAll(challanDetails?.businessService?.toUpperCase(), ".", "_")}`)}`} textStyle={{ whiteSpace: "pre" }} />
+            <Row label={`${t("UC_FROM_DATE_LABEL")}:`} text={convertEpochToDate(challanDetails?.taxPeriodFrom)} />
+            <Row label={`${t("UC_TO_DATE_LABEL")}:`} text={convertEpochToDate(challanDetails?.taxPeriodTo)} />
+            <Row label={`${t("UC_COMMENT_LABEL")}:`} text={`${challanDetails?.description}` || "NA"} />
+            <Row label={`${t("CS_INBOX_STATUS_FILTER")}:`} text={t(`UC_${challanDetails?.applicationStatus}`)} />
+          </StatusTable>
+          <CardSubHeader>{t("UC_CONSUMER_DETAILS_LABEL")}</CardSubHeader>
+          <StatusTable>
+            <Row label={`${t("UC_CONS_NAME_LABEL")}:`} text={challanDetails?.citizen.name} />
+            <Row label={`${t("UC_MOBILE_NUMBER")}:`} text={challanDetails?.citizen.mobileNumber} />
+            <Row label={`${t("UC_DOOR_NO_LABEL")}:`} text={challanDetails?.address.doorNo} />
+            <Row label={`${t("UC_BUILDING_NAME_LABEL")}:`} text={challanDetails?.address.buildingName} />
+            <Row label={`${t("UC_STREET_NAME_LABEL")}:`} text={challanDetails?.address.street} />
+            <Row label={`${t("UC_MOHALLA_LABEL")}:`} text={`${t(`${stringReplaceAll((challanDetails?.address?.tenantId?.toUpperCase()), ".", "_")}_REVENUE_${challanDetails?.address?.locality?.code}`)}`} />
+          </StatusTable>
         </Card>
       </div>
     </React.Fragment>
--- packages/modules/mCollect/src/utils/index.js
+++ packages/modules/mCollect/src/utils/index.js
@@ -28,3 +28,24 @@ export const getActionButton = (businessService, receiptNumber) => {
         > {t(`${"UC_DOWNLOAD_RECEIPT"}`)} </a>
     )
 }
+
+export const stringReplaceAll = (str = "", searcher = "", replaceWith = "") => {
+    if (searcher == "") return str;
+    while (str.includes(searcher)) {
+        str = str.replace(searcher, replaceWith);
+    }
+    return str;
+};
+
+export const convertEpochToDate = dateEpoch => {
+    if (dateEpoch == null || dateEpoch == undefined || dateEpoch == '') {
+        return "NA";
+    }
+    const dateFromApi = new Date(dateEpoch);
+    let month = dateFromApi.getMonth() + 1;
+    let day = dateFromApi.getDate();
+    let year = dateFromApi.getFullYear();
+    month = (month > 9 ? "" : "0") + month;
+    day = (day > 9 ? "" : "0") + day;
+    return `${day}/${month}/${year}`;
+};
