48337a1f7b196255131633001ae85ef8f8552782
harit-egov <harit.saxena@egovernments.org>
2021-06-09 06:28:32 +0530

    removed link for quick added search and pay in PT

--- packages/modules/pt/src/pages/citizen/index.js
+++ packages/modules/pt/src/pages/citizen/index.js
@@ -1,6 +1,6 @@
 import { AppContainer, BackButton, PrivateRoute } from "@egovernments/digit-ui-react-components";
 import React from "react";
-import { Redirect, Switch, useRouteMatch } from "react-router-dom";
+import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 import CreateProperty from "./Create";
 import { PTMyApplications } from "./PTMyApplications";
 import { MyProperties } from "./MyProperties/index";
@@ -23,10 +23,10 @@ const App = () => {
     <span className={"pt-citizen"}>
       <Switch>
         <AppContainer>
-          {!shouldHideBackButton(hideBackButtonConfig) ? <BackButton >Back</BackButton> : ""}
+          {!shouldHideBackButton(hideBackButtonConfig) ? <BackButton>Back</BackButton> : ""}
           <PrivateRoute path={`${path}/property/new-application`} component={CreateProperty} />
           <PrivateRoute path={`${path}/property/edit-application`} component={EditProperty} />
-          <PrivateRoute path={`${path}/property/search`} component={SearchPropertyComponent} />
+          <Route path={`${path}/property/search`} component={SearchPropertyComponent} />
           <PrivateRoute path={`${path}/property/search-results`} component={SearchResultsComponent} />
           <PrivateRoute path={`${path}/property/application/:acknowledgementIds`} component={PTApplicationDetails}></PrivateRoute>
           <PrivateRoute path={`${path}/property/my-applications`} component={PTMyApplications}></PrivateRoute>
