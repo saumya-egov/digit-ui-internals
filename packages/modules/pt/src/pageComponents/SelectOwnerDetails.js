322d01459a361619af497f4170dde3fcc1d02e48
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-06-11 19:34:53 +0530

    RAIN-2619 : Common Added Others gender in UI (#899)

--- packages/modules/pt/src/pageComponents/SelectOwnerDetails.js
+++ packages/modules/pt/src/pageComponents/SelectOwnerDetails.js
@@ -60,6 +60,7 @@ const SelectOwnerDetails = ({ t, config, onSelect, userType, formData }) => {
     { name: "Female", value: "FEMALE", code: "FEMALE" },
     { name: "Male", value: "MALE", code: "MALE" },
     { name: "Transgender", value: "TRANSGENDER", code: "TRANSGENDER" },
+    { name: "OTHERS", value: "OTHERS", code: "OTHERS" },
     // { name: "Other", value: "OTHER", code: "OTHER" },
   ];
 
