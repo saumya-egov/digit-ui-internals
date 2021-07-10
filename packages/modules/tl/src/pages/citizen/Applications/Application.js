5fd4b1d712ba708706f7ab2040b35dfec95c4d6c
Jagankumar-Egov <jagan.kumar@egovernments.org>
2021-07-10 08:30:56 +0530

    RAIN-2888 : HRMS jurisdiction issue in edit use case

--- packages/modules/tl/src/pages/citizen/Applications/Application.js
+++ packages/modules/tl/src/pages/citizen/Applications/Application.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Card, KeyNote, SubmitBar, Loader, Header } from "@egovernments/digit-ui-react-components";
+import { Card, Header, KeyNote, Loader, SubmitBar } from "@egovernments/digit-ui-react-components";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
