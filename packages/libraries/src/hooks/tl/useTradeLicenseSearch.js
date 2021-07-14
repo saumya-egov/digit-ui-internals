5ca6bf019d786e661f800264ed66b2d4dbc1d455
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2021-07-14 20:10:15 +0530

    RAIN-3052: Localization call added in seach API hook (#1048)

--- packages/libraries/src/hooks/tl/useTradeLicenseSearch.js
+++ packages/libraries/src/hooks/tl/useTradeLicenseSearch.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useQuery, useQueryClient } from "react-query";
 
 const useTradeLicenseSearch = ({ tenantId, filters, auth }, config = {}) => {
@@ -5,6 +6,10 @@ const useTradeLicenseSearch = ({ tenantId, filters, auth }, config = {}) => {
 
   const args = tenantId ? { tenantId, filters, auth } : { filters, auth };
   const { isLoading, error, data } = useQuery(["tradeSearchList", tenantId, filters], () => Digit.TLService.TLsearch(args), config);
+  useEffect (() => {
+    if(config?.filters?.tenantId)
+    Digit.LocalizationService.getLocale({modules: [`rainmaker-${config.filters?.tenantId}`], locale: Digit.StoreData.getCurrentLanguage(), tenantId: `${config?.filters?.tenantId}`});
+  },[config?.filters?.tenantId]);
 //   if (!isLoading && data && data?.Properties && Array.isArray(data.Properties) && data.Properties.length > 0) {
 //     data.Properties[0].units = data.Properties[0].units || [];
 //     data.Properties[0].units = data.Properties[0].units.filter((unit) => unit.active);
