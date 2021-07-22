9b37aea19b1238f6f1c6cf91910f3577d537ea10
saurabh-egov <saurabh.s@egovernments.org>
2021-07-22 09:23:00 +0530

    fixed topbar css, some overall wrapper css changed and module cards fixed

--- packages/modules/core/src/components/TopBarSideBar/TopBar.js
+++ packages/modules/core/src/components/TopBarSideBar/TopBar.js
@@ -48,7 +48,7 @@ const TopBar = ({
       <div className="topbar">
         {mobileView ? <Hamburger handleClick={toggleSidebar} color="#9E9E9E"/> : null}
         <img className="city" src={cityDetails?.logoId} />
-        <p className="ulb" style={mobileView ? { fontSize: "14px", display: "inline-block", marginTop: "15px" } : {}}>
+        <p className="ulb" style={mobileView ? { fontSize: "14px", display: "inline-block" } : {}}>
           {t(cityDetails?.i18nKey).toUpperCase()}{" "}
           {t(`ULBGRADE_${cityDetails?.city?.ulbGrade.toUpperCase().replace(" ", "_").replace(".", "_")}`).toUpperCase()}
         </p>
